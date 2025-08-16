67cecdd3a7d7bb7916eab4c4c5d67421fc62b5a9
Ashutoshdash26 <dash98829@gmail.com>
2025-08-16 19:33:17 +0530

    py jokes pyttsx3

--- student.py
+++ student.py
@@ -3,3 +3,26 @@ import pyjokes
 joke=pyjokes.get_joke()
 print("Printing jokes ... ")
 print(joke)
+import pyttsx3
+
+# Initialize the TTS engine
+engine = pyttsx3.init()
+
+# Set properties (optional)
+engine.setProperty('rate', 150)  # Speed of speech
+engine.setProperty('volume', 0.9)  # Volume (0.0 to 1.0)
+
+# Convert text to speech
+engine.say("Hi my name is Ashutosh Dash ")
+engine.runAndWait()
+
+
+import os
+
+# Specify the directory (you can change this to any path)
+directory =  "/Users\KIIT\Music\list"
+
+# List all files and folders in the directory
+print(f"Contents of directory: {os.path.abspath(directory)}\n")
+for item in os.listdir(directory):
+    print(item)
