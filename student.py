4fe09dbb0249fcc07f6385952b91b2c5459de3ba
Ashutoshdash26 <dash98829@gmail.com>
2025-08-16 19:34:58 +0530

    py jokes pyttsx3

--- student.py
+++ student.py
@@ -8,7 +8,6 @@ import pyttsx3
 
 engine = pyttsx3.init()
 
-# Set properties (optional)
 engine.setProperty('rate', 150)  # Speed of speech
 engine.setProperty('volume', 0.9)  # Volume (0.0 to 1.0)
 
