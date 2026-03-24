e452c7dc29b9133714dfa58f11fd373655df38cc
Ashutoshdash26 <dash98829@gmail.com>
2026-03-24 17:16:42 +0530

    Basic

---
+++ basic/abb.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "5103f2a5-6252-4e5f-b350-73cfb674b4af",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
