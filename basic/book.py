d0a9141d98b07990ac5546a7bc2588a2d2f26ef8
Ashutoshdash26 <dash98829@gmail.com>
2026-03-26 20:42:30 +0530

    Basic

---
+++ basic/book.py
@@ -0,0 +1,21 @@
+class Book:
+    def __init__(self, title):
+        self.title = title
+        self.available = True
+
+    def borrow(self):
+        if self.available:
+            self.available = False
+            print(f"{self.title} borrowed")
+        else:
+            print("Not available")
+
+    def return_book(self):
+        self.available = True
+        print(f"{self.title} returned")
+
+
+b1 = Book("Python Basics")
+b1.borrow()
+b1.borrow()
+b1.return_book()
