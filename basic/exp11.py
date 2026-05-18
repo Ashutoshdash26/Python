f7ea8338f07cc6f755f5915779ecfb1ff149359d
Ashutoshdash26 <dash98829@gmail.com>
2026-05-18 20:43:33 +0530

    Basic

--- basic/exp11.py
+++ basic/exp11.py
@@ -1,14 +1,17 @@
 
-def say(name="Ashutosh Dash "):
-    global count
-    count=0
-    def talk():
-        global count
-        count=count+1
-        print(f"Hi {name}, count is {count}")
-    talk()
+# def say(name="Ashutosh Dash "):
+#     global count
+#     count=0
+#     def talk():
+#         global count
+#         count=count+1
+#         print(f"Hi {name}, count is {count}")
+#     talk()
 
-count=10
-say()
+# count=10
+# say()
 
-    
+
+if(False & True):
+    print("A")
+print("b")
