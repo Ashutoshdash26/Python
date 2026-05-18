7f33a43a23023474d53d42d3d4f5799b4d055180
Ashutoshdash26 <dash98829@gmail.com>
2026-05-18 20:45:00 +0530

    Basic

--- basic/exp11.py
+++ basic/exp11.py
@@ -15,3 +15,15 @@
 if(False & True):
     print("A")
 print("b")
+
+# def say(name="Ashutosh Dash "):
+#     global count
+#     count=0
+#     def talk():
+#         global count
+#         count=count+1
+#         print(f"Hi {name}, count is {count}")
+#     talk()
+
+# count=10
+# say()
