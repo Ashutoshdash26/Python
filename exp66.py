59f1fbda9cec86bd1f82512eda327decba69c94f
Ashutoshdash26 <dash98829@gmail.com>
2026-06-11 17:33:45 +0530

    Exp

--- exp66.py
+++ exp66.py
@@ -5,12 +5,34 @@
 # print(name + " is my name")
 # print(abs(-5.7))
 # print(round(5.54))
-from enum import Enum
-class State(Enum):
-    INCATIVE=0
-    ACTIVE=1
-print(State["ACTIVE"].value)
-print(State.ACTIVE)         # State.ACTIVE
-print(State.ACTIVE.value)   # 1
-print(State["ACTIVE"])      # State.ACTIVE
-print(State["ACTIVE"].value)# 1
+
+###enum
+# from enum import Enum
+# class State(Enum):
+#     INCATIVE=0
+#     ACTIVE=1
+# print(State["ACTIVE"].value)
+# print(State.ACTIVE)         # State.ACTIVE
+# print(State.ACTIVE.value)   # 1
+# print(State["ACTIVE"])      # State.ACTIVE
+# print(State["ACTIVE"].value)# 1
+
+# print(list(State))
+
+
+### User input 
+
+
+# age=int(input("Enter your age : "))
+# print(f"Your age is {age}")
+# print(type(age))
+
+## function 
+
+
+def hello(name="Ashutosh ", age ="23"):
+    print(f"my name is {name} and age is {age}")
+
+hello()
+hello("Parth")
+hello("parth",23)
